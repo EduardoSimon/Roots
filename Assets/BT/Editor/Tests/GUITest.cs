dfc05d0a952b9fe3d99d11399477dd2a808e8258
Edu Simon <edusimonpicon@gmail.com>
2019-06-01 18:59:15 +0200

    [ADD] Added new connections Tests

--- Assets/BT/Editor/Tests/GUITest.cs
+++ Assets/BT/Editor/Tests/GUITest.cs
@@ -15,7 +15,7 @@ namespace GUITests
     public class GUITest
     {
         private BtEditor editor;
-        
+
         [SetUp]
         public void SetupEnvironment()
         {
@@ -33,7 +33,7 @@ namespace GUITests
             editor.Close();
             Assert.That(editor == null);
         }
-        
+
         [Test]
         [UnityPlatform(RuntimePlatform.WindowsEditor)]
         public void Window_is_not_null()
@@ -48,7 +48,7 @@ namespace GUITests
         {
             Assert.That(editor.entry != null);
         }
-        
+
         [Test]
         [TestCase(0)]
         [TestCase(1)]
@@ -61,8 +61,10 @@ namespace GUITests
         {
             for (int i = 0; i < n; i++)
             {
-                editor.CreateNodeView(new SearchTasksWindow.NodeType(typeof(Sequence),typeof(SequenceNode)), new Rect(100, 100, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
+                editor.CreateNodeView(new SearchTasksWindow.NodeType(typeof(Sequence), typeof(SequenceNode)),
+                    new Rect(100, 100, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
             }
+
             Assert.That(editor.Nodes.Count == n);
         }
 
@@ -78,11 +80,11 @@ namespace GUITests
         {
             for (int i = 0; i < n; i++)
             {
-                editor.CreateNodeView(new SearchTasksWindow.NodeType(typeof(Sequence),typeof(SequenceNode)), new Rect(100, 100, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
+                editor.CreateNodeView(new SearchTasksWindow.NodeType(typeof(Sequence), typeof(SequenceNode)),
+                    new Rect(100, 100, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
                 Assert.That(editor.Nodes[i].Task != null);
                 Assert.That(editor.Nodes[i] != null);
             }
-
         }
 
         [Test]
@@ -93,10 +95,69 @@ namespace GUITests
         [TestCase(typeof(PatrolNode), typeof(Patrol))]
         public void Can_add_node_of_type(Type drawerType, Type taskType)
         {
-            editor.CreateNodeView(new SearchTasksWindow.NodeType(taskType,drawerType), new Rect(100, 100, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
+            editor.CreateNodeView(new SearchTasksWindow.NodeType(taskType, drawerType),
+                new Rect(100, 100, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
             Assert.That(editor.Nodes[0].GetType() == drawerType);
             Assert.That(editor.Nodes[0].Task.GetType() == taskType);
         }
+
+        [Test]
+        [TestCase(typeof(SequenceNode), typeof(Sequence))]
+        [TestCase(typeof(DefaultNode), typeof(Selector))]
+        [TestCase(typeof(SeekNode), typeof(Seek))]
+        [TestCase(typeof(LogNode), typeof(Log))]
+        [TestCase(typeof(PatrolNode), typeof(Patrol))]
+        public void Set_root_view(Type drawerType, Type taskType)
+        {
+            editor.CreateNodeView(new SearchTasksWindow.NodeType(taskType, drawerType),
+                new Rect(100, 100, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
+
+            NodeSocket.CurrentClickedSocket = editor.entry.exitSocket;
+            
+            editor.OnNodeSocketClicked(editor.Nodes[0].entrySocket);
+            
+            Assert.That(editor.Connections[0].StartSocket != null);
+            Assert.That(editor.Connections[0].EndSocket != null);
+            Assert.That(editor.Connections.Count == 1);
+        }
         
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        [TestCase(6)]
+        [TestCase(20)]
+        [TestCase(40)]
+        public void Create_n_sequences_connected_with_one_as_root(int n)
+        {
+            editor.CreateNodeView(new SearchTasksWindow.NodeType(typeof(Sequence), typeof(SequenceNode)),
+                new Rect(100, 100, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
+
+            NodeSocket.CurrentClickedSocket = editor.entry.exitSocket;
+            
+            editor.OnNodeSocketClicked(editor.Nodes[0].entrySocket);
+            
+            Assert.That(editor.Connections[0].StartSocket != null);
+            Assert.That(editor.Connections[0].EndSocket != null);
+            Assert.That(editor.Connections.Count == 1);
+
+            for (int i = 0; i < n; i++)
+            {
+                editor.CreateNodeView(new SearchTasksWindow.NodeType(typeof(Sequence), typeof(SequenceNode)),
+                    new Rect(100 + i, 100 + i, BTConstants.kNodeWidht, BTConstants.kNodeHeight));
+                
+                NodeSocket.CurrentClickedSocket = editor.Nodes[editor.Nodes.Count - 2].exitSocket;
+                editor.OnNodeSocketClicked(editor.Nodes[editor.Nodes.Count - 1].entrySocket);
+                
+                Assert.That(editor.Connections[i].StartSocket != null);
+                Assert.That(editor.Connections[i].EndSocket != null);
+            }
+            
+            Assert.That(editor.Connections.Count == n + 1);
+
+            
+            
+        }
     }
 }
